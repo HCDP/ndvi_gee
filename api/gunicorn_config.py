bcf787171eee1f8be1f88d0ca8cce0722cffe7a5
jardmclan <mcleanj@hawaii.edu>
2024-07-18 13:50:25 -1000

    back to http

--- api/gunicorn_config.py
+++ api/gunicorn_config.py
@@ -1,10 +1,11 @@
 import os
 
-certfile = os.environ.get('GUNICORN_CERTFILE', None)
-keyfile = os.environ.get('GUNICORN_KEYFILE', None)
+# certfile = os.environ.get('GUNICORN_CERTFILE', None)
+# keyfile = os.environ.get('GUNICORN_KEYFILE', None)
 workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:443')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:80')
 
 forwarded_allow_ips = '*'
+# secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
