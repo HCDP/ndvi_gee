d24d4d3a64d2cf995632d4e217f652a23d22a623
jardmclan <mcleanj@hawaii.edu>
2024-07-18 13:39:00 -1000

    https

--- api/gunicorn_config.py
+++ api/gunicorn_config.py
@@ -5,7 +5,6 @@ keyfile = os.environ.get('GUNICORN_KEYFILE', None)
 workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:443')
 
 forwarded_allow_ips = '*'
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
